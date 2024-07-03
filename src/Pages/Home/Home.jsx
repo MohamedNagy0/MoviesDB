dc22487af574095202c3fae604834efc8fb4d597
MohamedNagy0 <mohamednagy.mis@gmail.com>
2024-07-04 02:56:44 +0300

    Helemt

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Helmet } from "react-helmet";
 
 export default function Home() {
     return (
         <>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <Container className="pt-[60px] lg:pt-[70px]">
                 <Row>
                     <Col>Home</Col>
--- src/Pages/TV/TV.jsx
+++ src/Pages/TV/TV.jsx
@@ -1,8 +1,12 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { Helmet } from "react-helmet";
 
 export default function TV() {
     return (
         <>
+            <Helmet>
+                <title>TV</title>
+            </Helmet>
             <Container className="pt-[60px] lg:pt-[70px]">
                 <Row>
                     <Col>TV</Col>
