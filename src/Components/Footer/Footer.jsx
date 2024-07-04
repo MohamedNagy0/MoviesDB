0529ea1ba0c9de6248c44f7013c8b1ab3f9a477f
MohamedNagy0 <mohamednagy.mis@gmail.com>
2024-07-04 10:56:53 +0300

    handel footer color

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,8 @@
 export default function Footer() {
     return (
-        <div className="bg-primary-bg border-t py-3 border-primary-border absolute bottom-0 w-full flex justify-center items-center">
+        <div className="bg-primary-bg-card1 text-primary-color2 border-t py-3 border-primary-border absolute bottom-0 w-full flex justify-center items-center">
             <span className="text-sm flex items-center gap-1">
-                <span className="text-primary-active">
-                    {" "}
-                    &copy; Mohamed Nagy
-                </span>
+                <span className="text-primary-active">&copy; Mohamed Nagy</span>
                 . All rights reserved
             </span>
         </div>
